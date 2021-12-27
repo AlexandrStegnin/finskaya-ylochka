1ecb5a56dbf8568d73f2241b0f0dd0cb1631c0d9
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-12-27 23:53:40 +0500

    Рефакторинг.

--- src/main/java/com/finskayaylochka/service/AppUserService.java
+++ src/main/java/com/finskayaylochka/service/AppUserService.java
@@ -5,7 +5,6 @@ import com.finskayaylochka.config.exception.EntityNotFoundException;
 import com.finskayaylochka.model.AppUser;
 import com.finskayaylochka.model.AppUser_;
 import com.finskayaylochka.model.UserProfile;
-import com.finskayaylochka.model.UserProfile_;
 import com.finskayaylochka.model.supporting.ApiResponse;
 import com.finskayaylochka.model.supporting.dto.UserDTO;
 import com.finskayaylochka.model.supporting.enums.KinEnum;
@@ -40,80 +39,75 @@ import java.util.UUID;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class AppUserService {
 
-    final AppUserRepository appUserRepository;
+  final AppUserRepository appUserRepository;
 
-    final PasswordEncoder passwordEncoder;
+  final PasswordEncoder passwordEncoder;
 
-    final AccountService accountService;
+  final AccountService accountService;
 
-    final MarketingTreeRepository marketingTreeRepository;
+  final MarketingTreeRepository marketingTreeRepository;
 
-    final AppUserSpecification specification;
+  final AppUserSpecification specification;
 
-    @PersistenceContext(name = "persistanceUnit")
-    EntityManager em;
+  @PersistenceContext(name = "persistanceUnit")
+  EntityManager em;
 
-    //    @Cacheable(Constant.USERS_CACHE_KEY)
-    public List<AppUser> findAll() {
-        return appUserRepository.findAll();
+  //    @Cacheable(Constant.USERS_CACHE_KEY)
+  public List<AppUser> findAll() {
+    return appUserRepository.findAll();
+  }
+
+  //    @CacheEvict(value = Constant.USERS_CACHE_KEY, key = "#id")
+  public void deleteUser(Long id) {
+    appUserRepository.deleteById(id);
+  }
+
+  public AppUser findByLogin(String login) {
+    return appUserRepository.findByLogin(login);
+  }
+
+  public AppUser findById(Long id) {
+    return appUserRepository.findOne(id);
+  }
+
+  public UserDTO find(UserDTO dto) {
+    AppUser user = appUserRepository.findOne(dto.getId());
+    if (user == null) {
+      throw new EntityNotFoundException("Пользователь не найден");
     }
+    return new UserDTO(user);
+  }
 
-    //    @CacheEvict(value = Constant.USERS_CACHE_KEY, key = "#id")
-    public void deleteUser(Long id) {
-        appUserRepository.deleteById(id);
-    }
+  public List<AppUser> initializeInvestors() {
+    return getUsers("Выберите инвестора");
+  }
 
-    public AppUser findByLogin(String login) {
-        return appUserRepository.findByLogin(login);
-    }
+  public List<AppUser> initializeMultipleInvestors() {
+    return findAll();
+  }
 
-    public AppUser findById(Long id) {
-        return appUserRepository.findOne(id);
-    }
+  public List<AppUser> initializePartners() {
+    return getUsers("Выберите партнёра");
+  }
 
-    public UserDTO find(UserDTO dto) {
-        AppUser user = appUserRepository.findOne(dto.getId());
-        if (user == null) {
-            throw new EntityNotFoundException("Пользователь не найден");
-        }
-        return new UserDTO(user);
-    }
+  private List<AppUser> getUsers(String s) {
+    AppUser partner = new AppUser();
+    partner.setId(0L);
+    partner.setLogin(s);
+    List<AppUser> users = new ArrayList<>(0);
+    users.add(partner);
+    users.addAll(findAll());
+    return users;
+  }
 
-    public void changeUserPassword(AppUser user, String password) {
-        user.setPassword(passwordEncoder.encode(password));
-        appUserRepository.saveAndFlush(user);
+  @Transactional
+  public void confirm(Long userId) {
+    AppUser investor = findById(userId);
+    if (!investor.isConfirmed()) {
+      investor.setConfirmed(true);
+      update(investor);
     }
-
-    public List<AppUser> initializeInvestors() {
-        return getUsers("Выберите инвестора");
-    }
-
-    public List<AppUser> initializeMultipleInvestors() {
-        return findAll();
-    }
-
-    public List<AppUser> initializePartners() {
-        return getUsers("Выберите партнёра");
-    }
-
-    private List<AppUser> getUsers(String s) {
-        AppUser partner = new AppUser();
-        partner.setId(0L);
-        partner.setLogin(s);
-        List<AppUser> users = new ArrayList<>(0);
-        users.add(partner);
-        users.addAll(findAll());
-        return users;
-    }
-
-    @Transactional
-    public void confirm(Long userId) {
-        AppUser investor = findById(userId);
-        if (!investor.isConfirmed()) {
-            investor.setConfirmed(true);
-            update(investor);
-        }
-    }
+  }
 
     /*
 
@@ -121,136 +115,129 @@ public class AppUserService {
 
      */
 
-    public AppUser findByLoginWithAnnexes(String login) {
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<AppUser> usersCriteriaQuery = cb.createQuery(AppUser.class);
-        Root<AppUser> usersRoot = usersCriteriaQuery.from(AppUser.class);
-        usersCriteriaQuery.select(usersRoot).distinct(true);
-        usersCriteriaQuery.where(cb.equal(usersRoot.get(AppUser_.login), login));
-        return em.createQuery(usersCriteriaQuery).getSingleResult();
-    }
+  public AppUser findByLoginWithAnnexes(String login) {
+    CriteriaBuilder cb = em.getCriteriaBuilder();
+    CriteriaQuery<AppUser> usersCriteriaQuery = cb.createQuery(AppUser.class);
+    Root<AppUser> usersRoot = usersCriteriaQuery.from(AppUser.class);
+    usersCriteriaQuery.select(usersRoot).distinct(true);
+    usersCriteriaQuery.where(cb.equal(usersRoot.get(AppUser_.login), login));
+    return em.createQuery(usersCriteriaQuery).getSingleResult();
+  }
 
-    //    @CachePut(value = Constant.USERS_CACHE_KEY, key = "#user?.id")
-    public ApiResponse create(AppUser user) {
-        ApiResponse response = new ApiResponse();
-        if (loginIsBusy(user.getLogin())) {
-            response.setError(String.format("Логин [%s] занят", user.getLogin()));
-            response.setStatus(HttpStatus.PRECONDITION_FAILED.value());
-            return response;
-        }
-        String password = generatePassword();
-        user.setPassword(passwordEncoder.encode(password));
-        user.getProfile().setUser(user);
-        user.getProfile().setEmail(user.getProfile().getEmail().toLowerCase());
-        if (Objects.isNull(user.getProfile().getFirstName())) {
-            user.getProfile().setFirstName("Инвестор " + user.getLogin().substring(8));
-        }
-        user.setLogin(user.getLogin().toLowerCase());
-        appUserRepository.save(user);
-        if (SecurityUtils.isUserInRole(user, UserRole.ROLE_INVESTOR)) {
-            accountService.createAccount(user);
-        }
-        response.setMessage("Пользователь успешно создан");
-        return response;
+  //    @CachePut(value = Constant.USERS_CACHE_KEY, key = "#user?.id")
+  public ApiResponse create(AppUser user) {
+    ApiResponse response = new ApiResponse();
+    if (loginIsBusy(user.getLogin())) {
+      response.setError(String.format("Логин [%s] занят", user.getLogin()));
+      response.setStatus(HttpStatus.PRECONDITION_FAILED.value());
+      return response;
     }
+    String password = generatePassword();
+    user.setPassword(passwordEncoder.encode(password));
+    user.getProfile().setUser(user);
+    user.getProfile().setEmail(user.getProfile().getEmail().toLowerCase());
+    if (Objects.isNull(user.getProfile().getFirstName())) {
+      user.getProfile().setFirstName("Инвестор " + user.getLogin().substring(8));
+    }
+    user.setLogin(user.getLogin().toLowerCase());
+    appUserRepository.save(user);
+    if (SecurityUtils.isUserInRole(user, UserRole.ROLE_INVESTOR)) {
+      accountService.createAccount(user);
+    }
+    response.setMessage("Пользователь успешно создан");
+    return response;
+  }
 
-    //    @CachePut(value = Constant.USERS_CACHE_KEY, key = "#user?.id")
-    public void updateProfile(AppUser user) {
-        AppUser dbUser = findById(user.getId());
-        if (null != user.getPassword()) {
-            dbUser.setPassword(passwordEncoder.encode(user.getPassword()));
-        }
-        if (null != user.getProfile().getEmail()) {
-            dbUser.getProfile().setEmail(user.getProfile().getEmail());
-        }
-        appUserRepository.save(dbUser);
+  //    @CachePut(value = Constant.USERS_CACHE_KEY, key = "#user?.id")
+  public void updateProfile(AppUser user) {
+    AppUser dbUser = findById(user.getId());
+    if (null != user.getPassword()) {
+      dbUser.setPassword(passwordEncoder.encode(user.getPassword()));
     }
+    if (null != user.getProfile().getEmail()) {
+      dbUser.getProfile().setEmail(user.getProfile().getEmail());
+    }
+    appUserRepository.save(dbUser);
+  }
 
-    //    @CachePut(value = Constant.USERS_CACHE_KEY, key = "#user?.id")
-    public ApiResponse update(AppUser user) {
-        AppUser dbUser = findById(user.getId());
-        dbUser.setPartner(user.getPartner());
-        dbUser.setKin(user.getKin());
-        dbUser.setRole(user.getRole());
-        UserProfile profile = dbUser.getProfile();
-        profile.setFirstName(user.getProfile().getFirstName());
-        profile.setLastName(user.getProfile().getLastName());
-        profile.setPatronymic(user.getProfile().getPatronymic());
-        profile.setEmail(user.getProfile().getEmail());
-        appUserRepository.save(dbUser);
-        return new ApiResponse("Пользователь успешно обновлён");
-    }
+  public ApiResponse update(AppUser user) {
+    AppUser dbUser = findById(user.getId());
+    dbUser.setPartner(user.getPartner());
+    dbUser.setKin(user.getKin());
+    dbUser.setRole(user.getRole());
+    updateProfile(user, dbUser);
+    appUserRepository.save(dbUser);
+    return new ApiResponse("Пользователь успешно обновлён");
+  }
 
-    public AppUser findByLoginAndEmail(String login, String email) {
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<AppUser> usersCriteriaQuery = cb.createQuery(AppUser.class);
-        Root<AppUser> usersRoot = usersCriteriaQuery.from(AppUser.class);
-        usersCriteriaQuery.select(usersRoot);
-        usersCriteriaQuery.where(cb.and(cb.equal(usersRoot.get(AppUser_.login), login),
-                cb.equal(usersRoot.get(AppUser_.profile).get(UserProfile_.email), email)));
-        return em.createQuery(usersCriteriaQuery).getSingleResult();
-    }
+  private void updateProfile(AppUser user, AppUser dbUser) {
+    UserProfile profile = dbUser.getProfile();
+    profile.setFirstName(user.getProfile().getFirstName());
+    profile.setLastName(user.getProfile().getLastName());
+    profile.setPatronymic(user.getProfile().getPatronymic());
+    profile.setEmail(user.getProfile().getEmail());
+  }
 
-    private String generatePassword() {
-        return UUID.randomUUID().toString().substring(0, 8);
-    }
+  private String generatePassword() {
+    return UUID.randomUUID().toString().substring(0, 8);
+  }
 
-    /**
-     * Проверка занят ли логин
-     *
-     * @param login логин для проверки
-     * @return статус
-     */
-    public boolean loginIsBusy(String login) {
-        return appUserRepository.existsByLogin(login);
-    }
+  /**
+   * Проверка занят ли логин
+   *
+   * @param login логин для проверки
+   * @return статус
+   */
+  public boolean loginIsBusy(String login) {
+    return appUserRepository.existsByLogin(login);
+  }
 
-    /**
-     * Деактивировать пользователя, чтоб убрать из маркетингового дерева
-     *
-     * @param dto DTO для деактивации
-     * @return ответ
-     */
-    public ApiResponse deactivateUser(UserDTO dto) {
-        if (dto.getId() == null) {
-            return new ApiResponse("Не задан id пользователя", HttpStatus.PRECONDITION_FAILED.value());
-        }
-        AppUser user = findById(dto.getId());
-        if (user == null) {
-            return new ApiResponse("Пользователь не найден", HttpStatus.PRECONDITION_FAILED.value());
-        }
-        user.setConfirmed(false);
-        user.setKin(KinEnum.EMPTY);
-        user.setPartner(null);
-        user.getProfile().setLocked(true);
-        update(user);
-        marketingTreeRepository.deleteByInvestorId(user.getId());
-        return new ApiResponse("Пользователь успешно деактивирован");
+  /**
+   * Деактивировать пользователя, чтоб убрать из маркетингового дерева
+   *
+   * @param dto DTO для деактивации
+   * @return ответ
+   */
+  public ApiResponse deactivateUser(UserDTO dto) {
+    if (dto.getId() == null) {
+      return new ApiResponse("Не задан id пользователя", HttpStatus.PRECONDITION_FAILED.value());
     }
+    AppUser user = findById(dto.getId());
+    if (user == null) {
+      return new ApiResponse("Пользователь не найден", HttpStatus.PRECONDITION_FAILED.value());
+    }
+    user.setConfirmed(false);
+    user.setKin(KinEnum.EMPTY);
+    user.setPartner(null);
+    user.getProfile().setLocked(true);
+    update(user);
+    marketingTreeRepository.deleteByInvestorId(user.getId());
+    return new ApiResponse("Пользователь успешно деактивирован");
+  }
 
-    public Page<AppUser> findAll(AppUserFilter filter, Pageable pageable) {
-        return appUserRepository.findAll(
-                specification.getFilter(filter),
-                pageable
-        );
-    }
+  public Page<AppUser> findAll(AppUserFilter filter, Pageable pageable) {
+    return appUserRepository.findAll(
+        specification.getFilter(filter),
+        pageable
+    );
+  }
 
-    public List<AppUser> getInvestors() {
-        return appUserRepository.getInvestors();
-    }
+  public List<AppUser> getInvestors() {
+    return appUserRepository.getInvestors();
+  }
 
-    public List<AppUser> getSellers() {
-        return appUserRepository.getSellers();
-    }
+  public List<AppUser> getSellers() {
+    return appUserRepository.getSellers();
+  }
 
-    public List<AppUser> initializeSellers() {
-        List<AppUser> appUsers = new ArrayList<>(200);
-        AppUser appUser = new AppUser();
-        appUser.setId(0L);
-        appUser.setLogin("Выберите продавца");
-        appUsers.add(appUser);
-        appUsers.addAll(getSellers());
-        return appUsers;
-    }
+  public List<AppUser> initializeSellers() {
+    List<AppUser> appUsers = new ArrayList<>(200);
+    AppUser appUser = new AppUser();
+    appUser.setId(0L);
+    appUser.setLogin("Выберите продавца");
+    appUsers.add(appUser);
+    appUsers.addAll(getSellers());
+    return appUsers;
+  }
 
 }
