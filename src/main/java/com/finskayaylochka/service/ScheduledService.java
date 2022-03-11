e957946701ab1cab0ba10535e4e00d45f15c21bd
Aleksandr Stegnin <aleksandr_stegnin@epam.com>
2022-03-11 20:15:16 +0500

    fix

--- src/main/java/com/finskayaylochka/service/ScheduledService.java
+++ src/main/java/com/finskayaylochka/service/ScheduledService.java
@@ -4,6 +4,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,7 +14,7 @@ public class ScheduledService {
 
     SendFilesService sendFilesService;
 
-//    @Scheduled(cron = "* */1 * * * ?")
+    @Scheduled(cron = "* */1 * * * ?")
     @SchedulerLock(name = "ScheduledService_sendFiles",// уникальное имя задачи
         lockAtLeastFor = "PT59S", // запускать не чаще, чем раз в 59 сек
         lockAtMostFor = "PT59S") // если нода "умерла" и не отпустила локу, то держит её не более 59 сек
