22775430310c2e660e2faa8423aae5e10d17d0cf
Aleksandr Stegnin <aleksandr_stegnin@epam.com>
2022-03-17 17:54:47 +0500

    Обновление счетов при обновлении объектов/подобъектов/помещений

--- src/main/java/com/finskayaylochka/service/FacilityService.java
+++ src/main/java/com/finskayaylochka/service/FacilityService.java
@@ -1,7 +1,8 @@
 package com.finskayaylochka.service;
 
+import com.finskayaylochka.config.exception.EntityNotFoundException;
+import com.finskayaylochka.model.Account;
 import com.finskayaylochka.model.Facility;
-import com.finskayaylochka.model.Facility_;
 import com.finskayaylochka.model.Money;
 import com.finskayaylochka.model.UnderFacility;
 import com.finskayaylochka.model.supporting.ApiResponse;
@@ -9,163 +10,120 @@ import com.finskayaylochka.model.supporting.dto.FacilityDTO;
 import com.finskayaylochka.model.supporting.enums.OwnerType;
 import com.finskayaylochka.repository.FacilityRepository;
 import com.finskayaylochka.repository.MoneyRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaUpdate;
-import javax.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Slf4j
 @Service
 @Transactional
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class FacilityService {
 
-    private final FacilityRepository facilityRepository;
+  UnderFacilityService underFacilityService;
+  FacilityRepository facilityRepository;
+  MoneyRepository moneyRepository;
+  AccountService accountService;
 
-    private final AccountService accountService;
+  public List<Facility> findAll() {
+    return facilityRepository.findAll();
+  }
 
-    private final MoneyRepository moneyRepository;
+  public Facility findById(Long id) {
+    return facilityRepository.findOne(id);
+  }
 
-    private final UnderFacilityService underFacilityService;
+  public List<Facility> initializeFacilities() {
+    List<Facility> facilitiesList = new ArrayList<>(0);
+    Facility facility = new Facility();
+    facility.setId(0L);
+    facility.setName("Выберите объект");
+    facilitiesList.add(facility);
+    facilitiesList.addAll(findAll());
+    return facilitiesList;
+  }
 
-    public FacilityService(FacilityRepository facilityRepository, AccountService accountService,
-                           MoneyRepository moneyRepository, UnderFacilityService underFacilityService) {
-        this.facilityRepository = facilityRepository;
-        this.accountService = accountService;
-        this.moneyRepository = moneyRepository;
-        this.underFacilityService = underFacilityService;
+  public List<Facility> initializeFacilitiesForMultiple() {
+    return findAll();
+  }
+
+  public Facility findByName(String name) {
+    return facilityRepository.findByName(name);
+  }
+
+  private void deleteById(Long id) {
+    facilityRepository.delete(id);
+    accountService.deleteByOwnerId(id, OwnerType.FACILITY);
+  }
+
+  private ApiResponse create(Facility facility) {
+    ApiResponse apiResponse = accountService.checkAccountNumber(facility);
+    if (Objects.nonNull(apiResponse)) {
+      return apiResponse;
     }
+    facilityRepository.saveAndFlush(facility);
+    accountService.createAccount(facility);
+    apiResponse = new ApiResponse("Объект " + facility.getName() + " успешно добавлен.", HttpStatus.OK.value());
+    return apiResponse;
+  }
 
-//    @Cacheable(Constant.FACILITIES_CACHE_KEY)
-    public List<Facility> findAll() {
-        return facilityRepository.findAll();
+  public ApiResponse delete(FacilityDTO dto) {
+    Facility facility = findById(dto.getId());
+    if (Objects.isNull(facility)) {
+      throw new EntityNotFoundException("Не найден объект для удаления");
     }
-
-    public Facility findById(Long id) {
-        return facilityRepository.findOne(id);
+    List<Money> monies = moneyRepository.findByFacilityId(facility.getId());
+    if (!monies.isEmpty()) {
+      return new ApiResponse(String.format("В объект [%s] вложены деньги, необходимо перераспределить их", facility.getName()), HttpStatus.BAD_REQUEST.value());
     }
-
-    public List<Facility> initializeFacilities() {
-        List<Facility> facilitiesList = new ArrayList<>(0);
-        Facility facility = new Facility();
-        facility.setId(0L);
-        facility.setName("Выберите объект");
-        facilitiesList.add(facility);
-        facilitiesList.addAll(findAll());
-        return facilitiesList;
+    try {
+      List<UnderFacility> underFacilities = underFacilityService.findByFacilityId(facility.getId());
+      underFacilities.forEach(underFacility -> underFacilityService.deleteById(underFacility.getId()));
+      deleteById(facility.getId());
+      return new ApiResponse("Объект " + facility.getName() + " успешно удалён.");
+    } catch (Exception e) {
+      log.error("Произошла ошибка: {}", e.getLocalizedMessage());
+      return new ApiResponse("При удалении объекта " + facility.getName() + " произошла ошибка.", HttpStatus.BAD_REQUEST.value());
     }
+  }
 
-    public List<Facility> initializeFacilitiesForMultiple() {
-        return findAll();
+  public ApiResponse update(FacilityDTO dto) {
+    Facility facility = new Facility(dto);
+    facilityRepository.save(facility);
+    updateAccount(facility);
+    return new ApiResponse("Объект успешно обновлён");
+  }
+
+  private void updateAccount(Facility facility) {
+    Account account = accountService.findByOwnerId(facility.getId(), OwnerType.FACILITY);
+    if (Objects.nonNull(account) && !facility.getName().equals(account.getOwnerName())) {
+      account.setOwnerName(facility.getName());
+      accountService.update(account);
     }
+  }
 
-    @PersistenceContext(name = "persistanceUnit")
-    private EntityManager em;
+  public ApiResponse create(FacilityDTO dto) {
+    Facility facility = new Facility(dto);
+    return create(facility);
+  }
 
-    public Facility findByName(String name) {
-        return facilityRepository.findByName(name);
-    }
-
-//    @CacheEvict(Constant.FACILITIES_CACHE_KEY)
-    public void deleteById(Long id) {
-        facilityRepository.delete(id);
-        accountService.deleteByOwnerId(id, OwnerType.FACILITY);
-    }
-
-//    @CachePut(value = Constant.FACILITIES_CACHE_KEY, key = "#facility.id")
-    public void update(Facility facility) {
-        CriteriaBuilder criteriaBuilder = this.em.getCriteriaBuilder();
-        CriteriaUpdate<Facility> update = criteriaBuilder.createCriteriaUpdate(Facility.class);
-        Root<Facility> facilityRoot = update.from(Facility.class);
-        update.set(Facility_.name, facility.getName());
-        update.set(Facility_.city, facility.getCity());
-        update.set(Facility_.fullName, facility.getFullName());
-        update.where(criteriaBuilder.equal(facilityRoot.get(Facility_.id), facility.getId()));
-        this.em.createQuery(update).executeUpdate();
-    }
-
-    /**
-     * Создать объект и счёт к нему
-     *
-     * @param facility объект
-     * @return ответ об успешности операции
-     */
-//    @Cacheable(Constant.FACILITIES_CACHE_KEY)
-    public ApiResponse create(Facility facility) {
-        ApiResponse apiResponse = accountService.checkAccountNumber(facility);
-        if (apiResponse != null) {
-            return apiResponse;
-        }
-        facilityRepository.saveAndFlush(facility);
-        accountService.createAccount(facility);
-        apiResponse = new ApiResponse("Объект " + facility.getName() + " успешно добавлен.", HttpStatus.OK.value());
-        return apiResponse;
-    }
-
-    /**
-     * Удалить объект на основании DTO
-     *
-     * @param dto DTO для удаления
-     * @return ответ
-     */
-    public ApiResponse delete(FacilityDTO dto) {
-        Facility facility = findById(dto.getId());
-        if (facility == null) {
-            throw new RuntimeException("Не найден объект для удаления");
-        }
-        List<Money> monies = moneyRepository.findByFacilityId(facility.getId());
-        if (monies.size() > 0) {
-            return new ApiResponse(String.format("В объект [%s] вложены деньги, необходимо перераспределить их", facility.getName()), HttpStatus.BAD_REQUEST.value());
-        }
-        try {
-            List<UnderFacility> underFacilities = underFacilityService.findByFacilityId(facility.getId());
-            underFacilities.forEach(underFacility -> underFacilityService.deleteById(underFacility.getId()));
-            deleteById(facility.getId());
-            return new ApiResponse("Объект " + facility.getName() + " успешно удалён.");
-        } catch (Exception e) {
-            log.error("Произошла ошибка: {}", e.getLocalizedMessage());
-            return new ApiResponse("При удалении объекта " + facility.getName() + " произошла ошибка.", HttpStatus.BAD_REQUEST.value());
-        }
-    }
-
-    /**
-     * Обновить объект на основе DTO
-     *
-     * @param dto DTO объекта
-     * @return ответ
-     */
-    public ApiResponse update(FacilityDTO dto) {
-        Facility facility = new Facility(dto);
-        facilityRepository.save(facility);
-        return new ApiResponse("Объект успешно обновлён");
-    }
-
-    /**
-     * Создать объект на основе DTO
-     *
-     * @param dto DTO объекта
-     * @return ответ
-     */
-    public ApiResponse create(FacilityDTO dto) {
-        Facility facility = new Facility(dto);
-        return create(facility);
-    }
-
-    /**
-     * Найти объекты, в которых у инвестора есть незакрытые суммы
-     *
-     * @param investorId id инвестора
-     * @return список объектов
-     */
-    public List<Facility> findOpenedProjects(Long investorId) {
-        return facilityRepository.findOpenedProjects(investorId);
-    }
+  /**
+   * Найти объекты, в которых у инвестора есть незакрытые суммы
+   *
+   * @param investorId id инвестора
+   * @return список объектов
+   */
+  public List<Facility> findOpenedProjects(Long investorId) {
+    return facilityRepository.findOpenedProjects(investorId);
+  }
 
 }
--- src/main/java/com/finskayaylochka/service/RoomService.java
+++ src/main/java/com/finskayaylochka/service/RoomService.java
@@ -23,82 +23,79 @@ import java.util.Objects;
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class RoomService {
 
-    MoneyService cashService;
-    AccountService accountService;
-    RoomRepository roomRepository;
+  MoneyService cashService;
+  AccountService accountService;
+  RoomRepository roomRepository;
 
-//    @Cacheable(Constant.ROOMS_CACHE_KEY)
-    public List<Room> findAll() {
-        List<Room> rooms = roomRepository.findAll();
-        rooms.forEach(room -> {
-            Hibernate.initialize(room.getUnderFacility());
-            Hibernate.initialize(room.getUnderFacility().getFacility());
-        });
-        return rooms;
-    }
+  public List<Room> findAll() {
+    List<Room> rooms = roomRepository.findAll();
+    rooms.forEach(room -> {
+      Hibernate.initialize(room.getUnderFacility());
+      Hibernate.initialize(room.getUnderFacility().getFacility());
+    });
+    return rooms;
+  }
 
-//    @Cacheable(Constant.ROOMS_CACHE_KEY)
-    public Room findById(Long id) {
-        Room room = roomRepository.findOne(id);
-        if (Objects.isNull(room)) {
-            throw new EntityNotFoundException("Не найдено помещение");
-        }
-        return room;
+  public Room findById(Long id) {
+    Room room = roomRepository.findOne(id);
+    if (Objects.isNull(room)) {
+      throw new EntityNotFoundException("Не найдено помещение");
     }
+    return room;
+  }
 
-    public Room findByIdWithUnderFacility(Long id) {
-        Room room = roomRepository.findOne(id);
-        Hibernate.initialize(room.getUnderFacility());
-        return room;
-    }
+  public Room findByRoom(String name) {
+    return roomRepository.findByName(name);
+  }
 
-//    @Cacheable(Constant.ROOMS_CACHE_KEY)
-    public Room findByRoom(String name) {
-        return roomRepository.findByName(name);
-    }
+  public ApiResponse deleteById(Long id) {
+    List<Money> cashes = cashService.findByRoomId(id);
+    cashes.forEach(c -> {
+      c.setRoom(null);
+      cashService.update(c);
+    });
+    roomRepository.delete(id);
+    accountService.deleteByOwnerId(id, OwnerType.ROOM);
+    return new ApiResponse("Помещение успешно удалено");
+  }
 
-//    @CacheEvict(value = Constant.ROOMS_CACHE_KEY)
-    public ApiResponse deleteById(Long id) {
-        List<Money> cashes = cashService.findByRoomId(id);
-        cashes.forEach(c -> {
-            c.setRoom(null);
-            cashService.update(c);
-        });
-        roomRepository.delete(id);
-        accountService.deleteByOwnerId(id, OwnerType.ROOM);
-        return new ApiResponse("Помещение успешно удалено");
-    }
+  public ApiResponse update(RoomDTO dto) {
+    Room room = new Room(dto);
+    roomRepository.saveAndFlush(room);
+    updateAccount(room);
+    return new ApiResponse("Помещение успешно обновлено");
+  }
 
-//    @CachePut(value = Constant.ROOMS_CACHE_KEY, key = "#room.id")
-    public ApiResponse update(RoomDTO dto) {
-        Room room = new Room(dto);
-        roomRepository.saveAndFlush(room);
-        return new ApiResponse("Помещение успешно обновлено");
+  private void updateAccount(Room room) {
+    Account account = accountService.findByOwnerId(room.getId(), OwnerType.ROOM);
+    if (Objects.nonNull(account) && !room.getName().equals(account.getOwnerName())) {
+      account.setOwnerName(room.getName());
+      accountService.update(account);
     }
+  }
 
-//    @CachePut(Constant.ROOMS_CACHE_KEY)
-    public void create(Room room) {
-        roomRepository.saveAndFlush(room);
-        UnderFacility underFacility = room.getUnderFacility();
-        Account account = accountService.findByOwnerId(underFacility.getId(), OwnerType.UNDER_FACILITY);
-        int countRooms = roomRepository.countByUnderFacilityId(underFacility.getId());
-        accountService.createAccount(room, account, countRooms);
-    }
+  private void create(Room room) {
+    roomRepository.saveAndFlush(room);
+    UnderFacility underFacility = room.getUnderFacility();
+    Account account = accountService.findByOwnerId(underFacility.getId(), OwnerType.UNDER_FACILITY);
+    int countRooms = roomRepository.countByUnderFacilityId(underFacility.getId());
+    accountService.createAccount(room, account, countRooms);
+  }
 
-    public ApiResponse create(RoomDTO dto) {
-        Room room = new Room(dto);
-        roomRepository.save(room);
-        return new ApiResponse("Помещение успешно создано");
-    }
+  public ApiResponse create(RoomDTO dto) {
+    Room room = new Room(dto);
+    create(room);
+    return new ApiResponse("Помещение успешно создано");
+  }
 
-    public List<Room> init() {
-        Room room = new Room();
-        room.setId(0L);
-        room.setName("Выберите помещение");
-        List<Room> roomList = new ArrayList<>(0);
-        roomList.add(room);
-        roomList.addAll(findAll());
-        return roomList;
-    }
+  public List<Room> init() {
+    Room room = new Room();
+    room.setId(0L);
+    room.setName("Выберите помещение");
+    List<Room> roomList = new ArrayList<>(0);
+    roomList.add(room);
+    roomList.addAll(findAll());
+    return roomList;
+  }
 
 }
